775da3150163a54b6d301be2bf7899649bfebd5e
Encritary <encritary@gmail.com>
2024-03-17 22:06:56 +0300

    fix: включён строгий режим выброса ошибок для mysqli
    это нужно для отлавливания mysqli_sql_exception

--- src/App.php
+++ src/App.php
@@ -10,6 +10,9 @@ use encritary\userQuests\db\Db;
 use encritary\userQuests\request\Request;
 use encritary\userQuests\response\Response;
 use encritary\userQuests\router\Router;
+use function mysqli_report;
+use const MYSQLI_REPORT_ERROR;
+use const MYSQLI_REPORT_STRICT;
 
 final class App{
 
@@ -17,6 +20,8 @@ final class App{
 
 	public function __construct(){
 		$config = Config::fromFile('config.json');
+
+		mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 		Db::init($config->dbCredentials);
 
 		ControllerFactory::init();
