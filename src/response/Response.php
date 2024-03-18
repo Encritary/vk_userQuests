9cb3d0a61289a12fd342c113d70c121e34641c21
Encritary <encritary@gmail.com>
2024-03-18 13:50:59 +0300

    fix: исправлено, что код ответа HTTP не учитывался в Response::echo()

--- src/response/Response.php
+++ src/response/Response.php
@@ -6,6 +6,7 @@ namespace encritary\userQuests\response;
 
 use encritary\userQuests\response\http\HttpCode;
 use encritary\userQuests\view\View;
+use function http_response_code;
 
 class Response{
 
@@ -15,6 +16,7 @@ class Response{
 	){}
 
 	public function echo() : void{
+		http_response_code($this->httpCode->value);
 		foreach($this->view->getHeaders() as $headerName => $val){
 			header($headerName . ': ' . $val);
 		}
