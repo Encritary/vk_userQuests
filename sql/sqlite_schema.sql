3bf84eb177995c3bfcc1075e5eac02d19ae314cd
Encritary <encritary@gmail.com>
2024-03-19 14:52:39 +0300

    docs: добавлена схема для sqlite

---
+++ sql/sqlite_schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users (
+    id integer primary key not null,
+    name varchar(32) not null,
+    balance int unsigned not null default 0
+);
+
+CREATE TABLE quests (
+    id integer primary key not null,
+    name varchar(128) not null,
+    cost int unsigned not null
+);
+
+CREATE TABLE user_completed_quests (
+    user_id integer unsigned not null,
+    quest_id integer unsigned not null,
+    completed_at timestamp not null default current_timestamp,
+    UNIQUE (user_id, quest_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (quest_id) REFERENCES quests (id)
+);
